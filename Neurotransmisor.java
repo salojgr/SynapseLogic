656dcf9822ab42f5e8cfb4fbfc6b35ffedbc56e3
VelasquezLuis <velasquez.luis@correo.unimet.edu.ve>
2026-05-03 17:46:40 -0400

    Add files via upload

---
+++ Neurotransmisor.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package neurotransmisor;
+
+/**
+ * Creación de clase Neurotransmisor con sus respectivos atributos.
+ */
+public class Neurotransmisor {
+    private final String id;
+    private final String nombre;
+    private final double velocidad;
+    private final String efecto;
+    private final String descripcion;
+    
+    // Constructor de la clase
+    public Neurotransmisor(String id, String nombre, double velocidad, String efecto, String descripcion){
+        this.id = id;
+        this.nombre = nombre;
+        this.velocidad = velocidad;
+        this.efecto = efecto;
+        this.descripcion = descripcion;    
+    }
+    
+    //Getters y Setters
+    public String getId(){
+        return id;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public double getVelocidad(){
+        return velocidad;
+    }
+    
+    public String getDescripcion(){
+        return descripcion;
+    }
+    
+    public String getEfecto(){
+        return efecto;
+    }
+    
+    //Representacuión textual de los atributos
+    @Override
+    public String toString(){
+        return "Neurotransmisor {" +
+                "ID='" + id + '\'' +
+                ", Nombre='" + nombre + '\'' +
+                ", Velocidad=" + velocidad +
+                ", Efecto='" + efecto + '\'' +
+                ", Descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
